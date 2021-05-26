a869d0c6326052d404f85b67502916a29eb2d277
crispinogian <81806139+crispinogian@users.noreply.github.com>
2021-05-26 10:20:15 +0800

    Add files via upload

---
+++ java/admin/testJavaFiile3.java
@@ -0,0 +1,28 @@
+/*                                                              
+ *******************************************************************************
+ *                                                                
+ * PROGRAM DESCRIPTION:                                           
+ *                                                                
+ * Test Java File 3
+ * 
+ * This file is a test java file for searching code snippets
+ *
+ * CODE REUSE TAGS
+ *
+ * #Landis
+ * #SIEMENS
+ *
+ *
+ *******************************************************************************
+ *                                                                    
+ *                                       
+ *******************************************************************************
+ */
+package com.com.com
+
+public class testJavaFile3 {
+
+	public static void main(){
+		
+	}
+}
---
+++ java/admin/testJavaFiile4.java
@@ -0,0 +1,28 @@
+/*                                                              
+ *******************************************************************************
+ *                                                                
+ * PROGRAM DESCRIPTION:                                           
+ *                                                                
+ * Test Java File 4
+ * 
+ * This file is a test java file for searching code snippets
+ *
+ * CODE REUSE TAGS
+ *
+ * #Landis
+ * #SIEMENS
+ *
+ *
+ *******************************************************************************
+ *                                                                    
+ *                                       
+ *******************************************************************************
+ */
+package com.com.com
+
+public class testJavaFile4 {
+
+	public static void main(){
+		
+	}
+}
